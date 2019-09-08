717c021c9fe13392884d6a47d7675fd761a238bf
Tuncay Aydin <tunikb@posteo.de>
2019-09-08 16:20:56 +0200

    fix: corrected typo in mapbox api token to show map on details page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<pk.eyJ1IjoidHVuaWtiIiwiYSI6ImNrMDZyMmJhdzE2dGgzYnBuM2Z6cmI0engifQ.Fse3iaPq5pMzR5qjlAvTGw>',
+        mapboxToken: 'pk.eyJ1IjoidHVuaWtiIiwiYSI6ImNrMDZyMmJhdzE2dGgzYnBuM2Z6cmI0engifQ.Fse3iaPq5pMzR5qjlAvTGw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
